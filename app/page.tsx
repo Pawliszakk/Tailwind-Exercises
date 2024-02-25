0abe56c80b2ffab80042767a88dbf406e1bb3795
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-02-26 00:01:23 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
-
 export default function Home() {
-	return <h1>hello</h1>;
+	return (
+		<>
+			<h1 className="bg-indigo-600 text-7xl container mx-auto">hello</h1>
+
+			<div className="m-64">
+				<p className="text-4xl">Lorem ipsum dolor sit.</p>
+				<p className="text-xs">Lorem ipsum dolor sit.</p>
+				<p className="text-5xl font-bold">Lorem ipsum dolor sit.</p>
+				<p>Lorem ipsum dolor sit.</p>
+			</div>
+		</>
+	);
 }
