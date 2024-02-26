9953dab4db414bc7d0e3435892747e33312231f9
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-02-26 23:02:28 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 		<>
 			<h1 className="bg-indigo-600 text-7xl container mx-auto">hello</h1>
 
-			<div className=" h-64 w-full my-20 bg-[url('./bgc.jpg')] "></div>
+			<div className="w-64 h-64 bg-indigo-400 rounded-lg m-64 shadow-xl opacity-50"></div>
 		</>
 	);
 }
