3a923c2b05b7610da7ff6c1dbc4becaa6801650b
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-02-26 22:38:24 +0100

    exercising with bgi

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,7 @@ export default function Home() {
 		<>
 			<h1 className="bg-indigo-600 text-7xl container mx-auto">hello</h1>
 
-			<div className="m-64">
-				<p className="text-4xl">Lorem ipsum dolor sit.</p>
-				<p className="text-xs">Lorem ipsum dolor sit.</p>
-				<p className="text-5xl font-bold">Lorem ipsum dolor sit.</p>
-				<p>Lorem ipsum dolor sit.</p>
-			</div>
+			<div className=" h-64 w-full my-20 bg-[url('./bgc.jpg')] "></div>
 		</>
 	);
 }
